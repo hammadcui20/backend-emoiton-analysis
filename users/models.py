5cb3e0c7b0208b6f7443c00b91c449c8ecbc67d4
hammadcui20 <hammadasmat@gmail.com>
2025-03-15 10:28:55 +0500

    Add dob field in db

--- users/models.py
+++ users/models.py
@@ -6,6 +6,7 @@ class User(AbstractUser):
     phone_number = models.CharField(max_length=12, blank=True, null=True)
     is_admin_user = models.BooleanField(default=False)
     is_analyst = models.BooleanField(default=True)
+    dob = models.DateField(null=True, blank=True)
 
     groups = models.ManyToManyField(Group, related_name="custom_user_groups", blank=True)
     user_permissions = models.ManyToManyField(Permission, related_name="custom_user_permissions", blank=True)
--- users/serializers.py
+++ users/serializers.py
@@ -7,7 +7,7 @@ from dj_rest_auth.serializers import LoginSerializer
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email']
+        fields = ['id', 'username', 'email', 'dob', 'phone_number']
 
 
 class RegisterSerializer(serializers.ModelSerializer):
@@ -15,14 +15,15 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password', 'phone_number']
+        fields = ['username', 'email', 'password', 'phone_number', 'dob']
 
     def create(self, validated_data):
         user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
             password=validated_data['password'],
-            phone_number=validated_data['phone_number']
+            phone_number=validated_data['phone_number'],
+            dob=validated_data['dob']
         )
         return user
 
